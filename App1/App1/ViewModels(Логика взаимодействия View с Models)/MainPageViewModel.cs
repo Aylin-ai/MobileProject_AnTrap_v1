0060036204bf6e11df5ccd9e4e1c2ac51da63719
Aylin-ai <ajlinkadan3224@gmail.com>
2023-02-05 14:17:26 +0300

    Создан отдельный класс, содержащий команды конкретной страницы; Создана новая страница UserPage

---
+++ App1/App1/ViewModels(Логика взаимодействия View с Models)/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using App1.ViewModels_Логика_взаимодействия_View_с_Models_.Base;
+using Xamarin.Forms;
+
+namespace App1.ViewModels_Логика_взаимодействия_View_с_Models_
+{
+    internal class MainPageViewModel: ViewModel
+    {
+        #region Заголовок страницы
+        private string _Title = "Главная страница";
+
+        /// <summary>
+        /// Заголовок страницы
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+        public Command cmdUserInfo { get; set; }
+
+        public MainPageViewModel()
+        {
+            cmdUserInfo = new Command(gotoNextPage);
+        }
+
+    }
+}
