000aa49f4e1ebbb3b495f18972930b8341117fc3
Aylin-ai <ajlinkadan3224@gmail.com>
2023-02-05 12:49:43 +0300

    Создан Базовый класс ViewModel

---
+++ App1/App1/ViewModels(Логика взаимодействия View с Models)/Base/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace App1.ViewModels_Логика_взаимодействия_View_с_Models_.Base
+{
+    internal abstract class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
