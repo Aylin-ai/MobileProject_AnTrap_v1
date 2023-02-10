a99c74df9e344bf6873595efb34c5e5f4c42d814
Aylin-ai <ajlinkadan3224@gmail.com>
2023-02-10 20:03:34 +0300

    Удалены Main_Book/Anime/Manga_Page; Добавлены ViewModel к страницам All_Anime/Manga/Books, Settings

---
+++ App1/App1/ViewModels(Логика взаимодействия View с Models)/Книги/AllBooksViewModel.cs
@@ -0,0 +1,11 @@
+﻿using App1.ViewModels_Логика_взаимодействия_View_с_Models_.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.ViewModels_Логика_взаимодействия_View_с_Models_.Книги
+{
+    internal class AllBooksViewModel : ViewModel
+    {
+    }
+}
