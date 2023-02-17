029fa3475148a03b077f44398ae3207fe8df0895
Aylin-ai <ajlinkadan3224@gmail.com>
2023-02-17 21:03:10 +0300

    Попытка использовать Http-запросы (Неудачная)

--- App1/App1/ViewModels(Логика взаимодействия View с Models)/Аниме/AllAnimeViewModel.cs
+++ App1/App1/ViewModels(Логика взаимодействия View с Models)/Аниме/AllAnimeViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using App1.ViewModels_Логика_взаимодействия_View_с_Models_.Base;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace App1.ViewModels_Логика_взаимодействия_View_с_Models_.Аниме
 {
     internal class AllAnimeViewModel : ViewModel
     {
+
+
+
     }
 }
--- TestCSharp/Program.cs
+++ TestCSharp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,45 @@ namespace TestCSharp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public record class Todo(string? Title = null);
+
+        private static HttpClient httpClient = new()
         {
+            BaseAddress = new Uri("https://animejoy.ru/"),
+        };
+
+        static async Task GetAsync(HttpClient httpClient)
+        {
+            using HttpResponseMessage response = await httpClient.GetAsync("todos/3");
+
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+
+            // Expected output:
+            //   GET https://jsonplaceholder.typicode.com/todos/3 HTTP/ 1.1
+            //   {
+            //     "userId": 1,
+            //     "id": 3,
+            //     "title": "fugiat veniam minus",
+            //     "completed": false
+            //   }
+        }
+
+        static async Task GetFromJsonAsync(HttpClient httpClient)
+        {
+            var todos = await httpClient.GetFromJsonAsync<List<Todo>>(
+                "todos?Title=Шпионский класс [07 из 12]");
+
+            Console.WriteLine("GET https://jsonplaceholder.typicode.com/todos?userId=1&completed=false HTTP/1.1");
+            todos?.ForEach(Console.WriteLine);
+            Console.WriteLine();
+        }
+
+        static async Task Main(string[] args)
+        {
+            await GetFromJsonAsync(httpClient);
+            Console.ReadLine();
         }
     }
 }
